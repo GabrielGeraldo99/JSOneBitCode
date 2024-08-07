73a5c85650dacafae4fba411db6f1d33189c484f
Gabriel Geraldo <gabrielgeraldo165@gmail.com>
2024-08-07 20:12:54 -0300

    Novas pastas e arquivos e mais um exercicio feito

---
+++ 1 - JS/3 - JSModerno/8 - tiposDeExport/non-inline.js
@@ -0,0 +1,18 @@
+/* Export nomeado e default em linhas diferentes da declaração */
+
+function group() {
+    console.log('Export nomeado não-inline (agrupado)')
+}
+
+function a() {}
+function b() {}
+function c() {}
+function d() {}
+
+function exportDefault() {
+  console.log('Export default não-inline')
+}
+
+export { group, a, b, c, d }
+
+export default exportDefault
